14c0c9da8c8ef07d5050a6a504eef3406f8421f2
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-05 17:29:36 -0500

    Actualizar main-menusearch.js

--- plugins/main-menusearch.js
+++ plugins/main-menusearch.js
@@ -10,3 +10,14 @@ let handler = async (m, { conn, args }) => {
     let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length;
 
     let txt = `
+Hola! Soy  *${botname}*
+Aquí tienes la lista de comandos
+╭┈ ↷
+│ᰔᩚ Cliente » @${userId.split('@')[0]}
+│❀ Modo » Publico
+│✦ Bot » ${(conn.user.jid == global.conn.user.jid ? 'Principal 🅥' : 'Sub Bot')}
+│ⴵ Activada » ${uptime}
+│✰ Usuarios » ${totalreg}
+│✧ Comandos » ${totalCommands}
+│🜸 Baileys » Multi Device
+╰─────────────────
