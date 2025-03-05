a4f4a17048f136e54f9feed80e177329632f5dd0
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-05 17:30:19 -0500

    Actualizar main-menusearch.js

--- plugins/main-menusearch.js
+++ plugins/main-menusearch.js
@@ -21,3 +21,38 @@ Aquí tienes la lista de comandos
 │✧ Comandos » ${totalCommands}
 │🜸 Baileys » Multi Device
 ╰─────────────────
+• :･ﾟ⊹˚• \`『 Menu-Search 』\` •˚⊹:･ﾟ•
+
+❍ Comandos para realizar búsquedas en distintas plataformas.
+ᰔᩚ *#tiktoksearch • #tiktoks*
+> ✦ Buscador de videos de tiktok.
+ᰔᩚ *#tweetposts*
+> ✦ Buscador de posts de Twitter/X.
+ᰔᩚ *#ytsearch • #yts*
+> ✦ Realiza búsquedas de Youtube.
+ᰔᩚ *#githubsearch*
+> ✦ Buscador de usuarios de GitHub.
+ᰔᩚ *#cuevana • #cuevanasearch*
+> ✦ Buscador de películas/series por Cuevana.
+ᰔᩚ *#google*
+> ✦ Realiza búsquedas por Google.
+ᰔᩚ *#pin • #pinterest*
+> ✦ Buscador de imagenes de Pinterest.
+ᰔᩚ *#imagen • #image*
+> ✦ buscador de imagenes de Google.
+ᰔᩚ *#animesearch • #animess*
+> ✦ Buscador de animes de tioanime.
+ᰔᩚ *#animei • #animeinfo*
+> ✦ Buscador de capítulos de #animesearch.
+ᰔᩚ *#infoanime*
+> ✦ Buscador de información de anime/manga.
+ᰔᩚ *#hentaisearch • #searchhentai*
+> ✦ Buscador de capítulos hentai.
+ᰔᩚ #xnxxsearch • #xnxxs*
+> ✦ Buscador de vídeos de Xnxx.
+ᰔᩚ *#xvsearch • #xvideossearch*
+> ✦ Buscador de vídeos de Xvideos.
+ᰔᩚ *#pornhubsearch • #phsearch*
+> ✦ Buscador de videos de Pornhub.
+ᰔᩚ *#npmjs*
+> ✦ Buscandor de npmjs.
