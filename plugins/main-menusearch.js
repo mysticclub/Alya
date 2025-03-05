5ebde1dab9074cc05a0335f7c1a2ee927e34c012
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-05 17:28:25 -0500

    Create main-menusearch.js

---
+++ plugins/main-menusearch.js
@@ -0,0 +1,12 @@
+import moment from 'moment-timezone';
+
+let handler = async (m, { conn, args }) => {
+    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+    let user = global.db.data.users[userId];
+    let name = conn.getName(userId);
+    let _uptime = process.uptime() * 1000;
+    let uptime = clockString(_uptime);
+    let totalreg = Object.keys(global.db.data.users).length;
+    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length;
+
+    let txt = `
